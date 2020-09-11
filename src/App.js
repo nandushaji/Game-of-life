a12d8aa0c16af454c284d8ff71643dc1f9f20333
nandushaji <nandushaji598@gmail.com>
2020-09-11 14:21:21 +0530

    bugs cleared

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ShowGrid from "./components/ShowGrid";
 import RowCol from "./components/RowCol";
-
+import cloneDeep from "lodash/cloneDeep";
 import "./App.css";
 
 const App = () => {
@@ -21,18 +21,75 @@ const App = () => {
     setGrid(initGrid);
   }, [row, col]);
 
+  useEffect(() => {
+    let interval;
+    if (start) {
+      interval = setInterval(() => {
+        if (!start) {
+          return;
+        }
+        setGrid(startLife);
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [start, grid, setGrid]);
+  const startLife = (grid) => {
+    let row = grid.length;
+    let col = grid[0].length;
+    const currentGen = cloneDeep(grid);
+    const copyGen = cloneDeep(grid);
+    const countNeighbours = (i, j, cGen) => {
+      var total = 0;
+      for (var xoff = -1; xoff <= 1; xoff++) {
+        for (var yoff = -1; yoff <= 1; yoff++) {
+          var r = i + xoff;
+          var c = j + yoff;
+          if (xoff !== 0 || yoff !== 0) {
+            if (r > -1 && r < row && c > -1 && c < col) {
+              if (cGen[r][c] === 1) {
+                total++;
+              }
+            }
+          }
+        }
+      }
+      return total;
+    };
+    for (let i = 0; i < row; i++) {
+      for (let j = 0; j < col; j++) {
+        var n = countNeighbours(i, j, currentGen);
+
+        if (currentGen[i][j] === 1) {
+          if (n < 2) {
+            copyGen[i][j] = 0;
+          } else if (n > 3) {
+            copyGen[i][j] = 0;
+          }
+        } else {
+          if (n === 3) {
+            copyGen[i][j] = 1;
+          }
+        }
+      }
+    }
+
+    setGrid(copyGen);
+  };
   return (
     <div className="App-header">
       <h1>Game of life</h1>
       <RowCol setCol={setCol} setRow={setRow} setGrid={setGrid} />
-      <ShowGrid
-        grid={grid}
-        row={row}
-        col={col}
-        setGrid={setGrid}
-        start={start}
-        SetStarted={SetStarted}
-      />
+      <ShowGrid grid={grid} row={row} col={col} setGrid={setGrid} />
+      <div style={{ textAlign: "center" }}>
+        <button
+          className="button"
+          onClick={() => {
+            SetStarted(!start);
+          }}
+        >
+          {start ? "stop" : "start"}
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/ShowGrid.js
+++ src/components/ShowGrid.js
@@ -2,50 +2,6 @@ import React, { useEffect } from "react";
 import cloneDeep from "lodash/cloneDeep";
 
 const ShowGrid = (props) => {
-  const startLife = (grid) => {
-    let row = grid.length;
-    let col = grid[0].length;
-
-    const currentGen = cloneDeep(props.grid);
-    const copyGen = cloneDeep(props.grid);
-    const countNeighbours = (i, j, cGen) => {
-      var total = 0;
-      for (var xoff = -1; xoff <= 1; xoff++) {
-        for (var yoff = -1; yoff <= 1; yoff++) {
-          var r = i + xoff;
-          var c = j + yoff;
-          if (xoff !== 0 || yoff !== 0) {
-            if (r > -1 && r < row && c > -1 && c < col) {
-              if (cGen[r][c] === 1) {
-                total++;
-              }
-            }
-          }
-        }
-      }
-      return total;
-    };
-    for (let i = 0; i < row; i++) {
-      for (let j = 0; j < col; j++) {
-        var n = countNeighbours(i, j, currentGen);
-
-        if (currentGen[i][j] === 1) {
-          if (n < 2) {
-            copyGen[i][j] = 0;
-          } else if (n > 3) {
-            copyGen[i][j] = 0;
-          }
-        } else {
-          if (n === 3) {
-            copyGen[i][j] = 1;
-          }
-        }
-      }
-    }
-
-    props.setGrid(copyGen);
-  };
-
   const lineStyle = {
     background: "#4CAF50",
     border: "none",
@@ -97,22 +53,6 @@ const ShowGrid = (props) => {
           ))}
         </tbody>
       </table>
-      <div style={{ textAlign: "center" }}>
-        <button
-          className="button"
-          onClick={() => {
-            const interval = 500;
-            props.SetStarted(!props.start);
-            setInterval(function () {
-              if (props.start) {
-                startLife(props.grid);
-              }
-            }, interval);
-          }}
-        >
-          {props.start ? "stop" : "start"}
-        </button>
-      </div>
     </div>
   );
 };
