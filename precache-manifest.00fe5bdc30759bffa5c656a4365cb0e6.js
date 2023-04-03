7a31f83d15ca3ac5e9711a48835c83f8d120dce6
Nandu-Shaji <nandu.s@keyvalue.systems>
2023-04-03 20:11:57 +0530

    Updates

---
+++ precache-manifest.00fe5bdc30759bffa5c656a4365cb0e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3017380aaf26927c67de126f6b8e40fd",
+    "url": "/Game-of-life/index.html"
+  },
+  {
+    "revision": "2cda881cebb05561c153",
+    "url": "/Game-of-life/static/css/main.a3a627e3.chunk.css"
+  },
+  {
+    "revision": "5a0b763e6a60d36dcc7e",
+    "url": "/Game-of-life/static/js/2.ba15851d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Game-of-life/static/js/2.ba15851d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cda881cebb05561c153",
+    "url": "/Game-of-life/static/js/main.13be9ee5.chunk.js"
+  },
+  {
+    "revision": "dd03eaa921fd48e91276",
+    "url": "/Game-of-life/static/js/runtime-main.ebc315ec.js"
+  }
+]);
