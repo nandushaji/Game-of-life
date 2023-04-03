23e17f1d0371232539fa28f84bf48a8d63e11b2e
Nandu-Shaji <nandu.s@keyvalue.systems>
2023-04-03 20:04:57 +0530

    added-home

---
+++ build/precache-manifest.9ddf1133c27fe0e213a87d592a945a99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f964247ffea3cdd224779592e0d52ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ac8cf479dc8b013e850",
+    "url": "/static/css/main.a3a627e3.chunk.css"
+  },
+  {
+    "revision": "5a0b763e6a60d36dcc7e",
+    "url": "/static/js/2.ba15851d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba15851d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac8cf479dc8b013e850",
+    "url": "/static/js/main.a5c7bb5e.chunk.js"
+  },
+  {
+    "revision": "3dfeed96ada7e9d3e73d",
+    "url": "/static/js/runtime-main.63eb4299.js"
+  }
+]);
